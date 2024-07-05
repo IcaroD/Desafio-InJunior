bf72fc3b11539172c5388d6190875a85f6462c69
IcaroD <icaro.davila.santos@gmail.com>
2024-07-05 15:09:52 -0300

    Exercicio extra 3

---
+++ ExercicioExtra3/ExercicioExtra3.js
@@ -0,0 +1,13 @@
+/* 
+    No Console do Navegador
+*/
+
+const valores = []
+const original = []
+for (var i = 0; i < 5; i++) {
+    var valor = parseInt(prompt(`Digite um valor inteiro ${i + 1}/5:`))
+    valores.push(valor)
+    original.push(valor)
+}
+
+alert(`Ordem crescente: ${valores.sort((a, b) => a-b)}\n\nSequência Original ${original}`)
